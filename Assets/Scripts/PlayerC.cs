e22da139eb65a11bd36a0f1951f409e542ec6ceb
Baxi19 <randald1991@gmail.com>
2021-07-16 18:06:21 -0600

    Animation of run and jump added :alien:

--- Assets/Scripts/PlayerC.cs
+++ Assets/Scripts/PlayerC.cs
@@ -5,22 +5,26 @@ using UnityEngine;
 public class PlayerC : MonoBehaviour
 {
     [SerializeField]
-    private float playerSpeed = 2.0f;
-    
+    private float playerSpeed = 7.0f;
+
     [SerializeField]
     private float jumpHeight = 1.0f;
-    
+
     [SerializeField]
     private float gravityValue = -9.81f;
-    
+
     [SerializeField]
     private float rotationSpeed = 4f;
+
+    [SerializeField]
+    public Animator playerAnimator;
     private CharacterController controller;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
     private PlayerControls playerInput;
     private Transform cameraMain;
     private Transform child;
+    bool couroutineStarted = false;
 
     private void Awake()
     {
@@ -51,32 +55,38 @@ public class PlayerC : MonoBehaviour
         if (groundedPlayer && playerVelocity.y < 0)
         {
             playerVelocity.y = 0f;
+            playerAnimator.SetBool("isJumping", false);
         }
 
         Vector2 movementInput = playerInput.PlayerMain.Move.ReadValue<Vector2>();
+        
+        if(movementInput != Vector2.zero){
+            playerAnimator.SetBool("isRunning", true);
+        }else{
+            playerAnimator.SetBool("isRunning", false);
+        }
+
         //Vector3 move = new Vector3(movementInput.x, 0f, movementInput.y); //Works with mouse
         Vector3 move = (cameraMain.forward * movementInput.y + cameraMain.right * movementInput.x);
         move.y = 0f;
         controller.Move(move * Time.deltaTime * playerSpeed);
 
-        /*if (move != Vector3.zero)
-        {
-            gameObject.transform.forward = move;
-        }*/
-
         // Changes the height position of the player..
         if (playerInput.PlayerMain.Jump.triggered && groundedPlayer)
         {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+            playerAnimator.SetBool("isJumping", true);
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue); 
         }
 
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
 
 
-        if(movementInput != Vector2.zero){
-            Quaternion rotation = Quaternion.Euler(new Vector3(child.localEulerAngles.x, cameraMain.localEulerAngles.y ,child.localEulerAngles.z ));
+        if (movementInput != Vector2.zero)
+        {
+            Quaternion rotation = Quaternion.Euler(new Vector3(child.localEulerAngles.x, cameraMain.localEulerAngles.y, child.localEulerAngles.z));
             child.rotation = Quaternion.Lerp(child.rotation, rotation, Time.deltaTime * rotationSpeed);
         }
     }
+
 }
